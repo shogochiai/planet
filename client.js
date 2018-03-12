7e218bf0fe229d66e789116ea36eb585796f5571
Shogo Ochiai <shogo1104@gmail.com>
2018-03-12 18:12:13 +0700

    Add removeCertificate

--- client.js
+++ client.js
@@ -1,38 +1,55 @@
 let lotion = require('lotion')
 let YOUR_APP_GCI = require('fs').readFileSync('.planet.gci').toString()
 
+function tx(am, ty, ac){
+  return { amount: am, type: ty, action: ac }
+}
+function inTx(am, ad, ty, ac){
+  let transaction = tx(am, ty, ac)
+  transaction.senderAddress = ad
+  return transaction
+}
+function outTx(am, ad, ty, ac){
+  let transaction = tx(am, ty, ac)
+  transaction.receiverAddress = ad
+  return transaction
+}
+
 let main = async _=>{
   let client = await lotion.connect(YOUR_APP_GCI)
   let results = []
+
+  // judd is an user
+  // matt is also an user
   let r1 = await client.send({
-    from: [
-      { amount: 4, senderAddress: 'judd', type: 'point', action: 'transfer' }
-    ],
-    to: [
-      { amount: 4, receiverAddress: 'matt', type: 'point', action: 'transfer' }
-    ]
+    from: [inTx(4,'judd','point','transfer')],
+    to: [outTx(4,'matt','point','transfer')]
   })
   results.push(r1)
 
+  // matt is applied user
+  // owen is administrator
   let r2 = await client.send({
-    from: [
-      { amount: 1, senderAddress: 'matt', type: 'certificate', action: 'apply' }
-    ],
-    to: [
-      { amount: 1, receiverAddress: 'owen', type: 'certificate', action: 'apply' }
-    ]
+    from: [inTx(1,'matt','certificate','apply')],
+    to: [outTx(1,'owen','certificate','apply')]
   })
   results.push(r2)
 
+  // owen is administrator
+  // matt is applied user
   let r3 = await client.send({
-    from: [
-      { amount: 1, senderAddress: 'owen', type: 'certificate', action: 'addCertificate' }
-    ],
-    to: [
-      { amount: 1, receiverAddress: 'matt', type: 'certificate', action: 'addCertificate' }
-    ]
+    from: [inTx(1,'owen','certificate','addCertificate')],
+    to:  [outTx(1,'matt','certificate','addCertificate')]
   })
   results.push(r3)
+
+  // owen is administrator
+  // matt is applied user
+  let r4 = await client.send({
+    from: [inTx(0,'owen','certificate','removeCertificate')],
+    to:  [outTx(0,'matt','certificate','removeCertificate')]
+  })
+  results.push(r4)
 
   return results
 }
--- contracts/certificate.js
+++ contracts/certificate.js
@@ -1,47 +1,81 @@
+const APPLIED = 'applied'
+const CERTIFIED = 'certified'
+const BLANK = 'blank'
 module.exports = {
   // Basically substate is closed within contract
   // Contract coin's tranfer can be within here, but global coin transfer is done by account modules of mappum/coins
   initialState: {
     administrators: {
       owen: {
+        balance: 5,
         expireBlockHeight: 200000
       }
     },
     users: {
+      matt: {
+        balance: 2
+      }
     }
   },
   actions: {
     apply: {
       onInput(input, tx, state){
-        state.users[input.senderAddress] = {
-          status: 'applied',
-          appliedBlockHeight: 80000,//current
-          expireBlockHeight: 100000//current+X
-        }
+        let user = state.users[input.senderAddress]
+        user.status = APPLIED
+        user.appliedBlockHeight = 80000//current
+        user.expireBlockHeight = 100000//current+X
+        
+        state.users[input.senderAddress] = user
       },
       onOutput(output, tx, state){}
     },
     addCertificate: {
       onInput(input, tx, state){
-        if(state.administrators[input.senderAddress] == null){
-          throw Error('This sender doesn\'t have any right to give certification!')
+        let admin = state.administrators[input.senderAddress]
+        if(admin == null){
+          throw Error('This sender doesn\'t have any right to give certificate!')
         }
+        if(input.amount !== 1){
+          throw Error('Certification cost is not 1, too few or too much.')
+        }
+        
+        admin.balance += 1 // TODO: Is it reasonable to add balance first before finish substracting? Feel like bad manner.
+
+        // apply state change
+        state.administrators[input.senderAddress] = admin
       },
       onOutput(output, tx, state){
         console.log('===============prestate', state)
-        if(state.users[output.receiverAddress] && state.users[output.receiverAddress].status !== 'applied'){
-          throw Error('Target user did not apply for this cerification.')
+        let user = state.users[output.receiverAddress]
+        if(user && user.status !== APPLIED){
+          throw Error('Target user is not applying for this cerificate.')
         }
+        if(user.balance < 1) {
+          throw Error('Not enough balance to get this certificate.')
+        }
+
+        user.balance -= 1
+        user.status = CERTIFIED
+        user.certifiedBlockHeight = 80001//current
+        user.expireBlockHeight = 120000// current + Y
         
-        state.users[output.receiverAddress].status = 'certified'
-        state.users[output.receiverAddress].certifiedBlockHeight = 80001//current
-        state.users[output.receiverAddress].expireBlockHeight = 120000// current + Y
+        // apply state change
+        state.users[output.receiverAddress] = user
         console.log('===============poststate', state)
       }
     },
     removeCertificate: {
-      onInput(input, tx, state){},
-      onOutput(output, tx, state){}
+      onInput(input, tx, state){
+        if(state.administrators[input.senderAddress] == null){
+          throw Error('This sender doesn\'t have any right to remove certificate!')
+        }
+      },
+      onOutput(output, tx, state){
+        if(!state.users[output.receiverAddress] || state.users[output.receiverAddress].status !== CERTIFIED){
+          throw Error('Target user is not certified on this cerificate.')
+        }
+        state.users[output.receiverAddress].status = BLANK
+      }
     },
     updateAdministraterExpiration: {
       onInput(input, tx, state){},
